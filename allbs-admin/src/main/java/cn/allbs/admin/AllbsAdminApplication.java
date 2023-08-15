2c4234d713ae4d82a7ed1e7d68773fbf71456954
chenqi <chenqi92104@icloud.com>
2023-08-15 13:32:56 +0800

    init

---
+++ allbs-admin/src/main/java/cn/allbs/admin/AllbsAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.allbs.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllbsAdminApplication.class, args);
+    }
+
+}
---
+++ allbs-admin/src/test/java/cn/allbs/admin/AllbsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.allbs.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllbsAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
